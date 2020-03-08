e9ed1051518d60a04a0789aadebb4360b8f12baf
amrit-k-daya <61942653+amrit-k-daya@users.noreply.github.com>
2020-03-08 18:39:36 +0000

    improved class definitions, made notes for necessary additions including new classes

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,14 @@
 #include <Servo.h>
 #include <Wire.h>
 
-
 /*=====CLASS DEFINITIONS=====*/
 class Sweeper
 {
   Servo servo;
-  int angle;
-  const int updateInterval = 15;
-  int lastUpdate;
+  int angle = 0;
   
   public:
-    Sweeper(int interval){}
+    Sweeper(){}
 
     void Attach(int pin)
     {
@@ -23,39 +20,49 @@ class Sweeper
       servo.detach();
     }
 
-    void Sweep()
+    void SweepOut()
     {
-      if((millis() - lastUpdate) > updateInterval)
-      {
-        lastUpdate = millis();
-        for(angle = 0; angle < 90; angle++){ 
-          servo.write(angle);
-        }
-        
+      for(angle = 0; angle < 65; angle++){ 
+        servo.write(angle);
+      }
+    }
+
+    void SweepIn()
+    {
+      for(angle = 65; angle > 0; angle--){
+        servo.write(angle);
       }
     }
 };
 
 class Wheel
 {
-  int encoderA;
-  int encoderB;
-  int motorPin;
+  int encoderAPin;
+  int encoderBPin;
   int pwmSpeed;
 
-  int getSpeed(){
-    int rpm;
-    //access encoder, run calculations
-    return rpm;
-  }
-
-  void moveForward(pwmSpeed){
-    //write pwm speed to motor
-  }
-
-  void brake(){
-    //write brake sequence to motor
-  }
+  public:
+    Wheel(int encoderA, int encoderB){
+      encoderAPin = encoderA;
+      encoderBPin = encoderB;
+      pinMode(encoderAPin, INPUT);
+      pinMode(encoderBPin, INPUT);
+      //need to find a way to connect I2C in this class (pins: SDA - 20, SCL - 21)
+    }
+    
+    int getSpeed(){
+      int rpm;
+      //access encoder, run calculations
+      return rpm;
+    }
+  
+    void moveForward(int pwmSpeed){
+      //write pwm speed to motor
+    }
+  
+    void brake(){
+      //write brake sequence to motor
+    }
 };
 
 class Extender
@@ -63,69 +70,83 @@ class Extender
   Servo servo;
   int fsrPin;
   int fsrData;
-  int angle;
+  int angle = 0;
 
-  void Attach(int pin)
-    {
-      servo.attach(pin);
+  public:
+    Extender(int forcePin){
+      fsrPin = forcePin;
+      pinMode(fsrPin, INPUT);
     }
-
-  void Detach()
-  {
-    servo.detach();
-  }
-   
-  int checkSensor(){
-    int force;
-    //get force reading from sensor
-    return force;
-  }
+    
+    void Attach(int pin)
+      {
+        servo.attach(pin);
+      }
   
-  void actuate(angle){
-    //write angle to servo;
-  }
-
-  void freeze(){
-    //stop servo
-  }
+    void Detach()
+    {
+      servo.detach();
+    }
+     
+    int checkSensor(){
+      int force;
+      //get force reading from sensor
+      return force;
+    }
+    
+    void actuate(int angle){
+      //write angle to servo;
+    }
+  
+    void freeze(){
+      //stop servo
+    }
 };
 
 class Valve
 {
   int valvePin;
 
-  void openValve(){
-    //actuation signal;
-  }
-
-  void closeValve(){
-    //stop signal;
-  }
+  public:
+    Valve(int pin){
+      valvePin = pin;
+    }
+    
+    void openValve(){
+      //actuation signal;
+    }
+  
+    void closeValve(){
+      //stop signal;
+    }
 };
 
 
 /*=====VAR INIT=====*/
-Rotator servo1(15);
-int infraPin = 0;
 
-Servo myServo;
+
 
 
 /*=====SETUP=====*/
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  myServo.attach(11);
+  /*~~~CREATE OBJS~~~*/
+  Sweeper seedServo, claw;
+  Extender digger(23), moistureServo(24);
+  Wheel left(8, 7), right(6, 5);
+  Valve waterOut(12);
+  /*~~~SET PINS~~~*/
+  int infraPin = 0;
+  /*~~~INIT~~~*/
   pinMode(infraPin, INPUT);
+  seedServo.Attach(22);
+  claw.Attach(11);
+  digger.Attach(10);
+  moistureServo.Attach(9);      //moisture sensor needs to be a class extending Extender class to allow moisture sensing as a method
+  
+  Serial.begin(9600);
 }
-
-
 
 /*=====MAIN=====*/
 void loop() {
-  // put your main code here, to run repeatedly:
-  //output = analogRead(infraPin);
-  //Serial.println(output);
-  myServo.write(90);
-  myServo.write(89);
+
 }
