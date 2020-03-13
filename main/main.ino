7b230c7fc0326b488abd73e9ea65df1dc2664d88
amrit-k-daya <61942653+amrit-k-daya@users.noreply.github.com>
2020-03-13 02:55:12 +0000

    Completed all classes but Wheels. Need to test with all components and make loop

--- main/main.ino
+++ main/main.ino
@@ -1,96 +1,99 @@
 #include <Servo.h>
 #include <Wire.h>
 
+/*positional variables*/
 int posX = 0;
 int posY = 0;
 int posZ = 0;
 
+/*soil force threshold qualities*/
+const int soil = 200;
+const int toughSoil = 400;
+
+/*pin assignment*/
 const byte forceClaw = 0;
-const byte forceMoisture;
-const byte clawPin = 11;
-const byte seedPin = 22;
-const byte diggerPin = 10;
-const byte moisturePin = 24;
+const byte forceMoist;
+/*-----------------------------*/
+const byte clawServoPin = 11;
+const byte seedServoPin = 22;
+const byte diggerServoPin = 10;
+const byte moistServoPin;
+/*-----------------------------*/
 const byte leftEncA = 8;
 const byte leftEncB = 7;
 const byte rightEncA = 6;
 const byte rightEncB = 5;
+/*-----------------------------*/
 const byte valvePin = 12;
-//const byte infraPin = 0;
+/*-----------------------------*/
+const byte infraPin = 0;
 
-
-
-Servo myServo;
-  /*~~~CREATE OBJS~~~*/
-  //Sweeper seeder(seedPin), claw(clawPin);
-  //Extender digger(diggerPin, forceClaw), moistureServo(moisturePin, forceMoisture);
-  //Wheel left(leftEncA, leftEncB), right(rightEncA, rightEncB);
-  //Valve waterOut(valvePin);
-/*=====SETUP=====*/
-void setup() {
-	
-	Serial.begin(9600);
-	myServo.attach(10);
-  myServo.write(0);
-
-	/*~~~INIT~~~*/
-	//pinMode(infraPin, INPUT);
-	//moistureServo.Attach(9);      //moisture sensor needs to be a class extending Extender class to allow moisture sensing as a method
-}
-
-/*=====MAIN=====*/
-void loop() {
-	y = analogRead(forceClaw);		//store measured force
-	e = r - y;		//e = r - y
-	u_angle = Kp*e;
-	
-	u_angle = bound(u_angle, -180, 180);
-	myServo.write(u_angle);
-	
-	Serial.print(y);
-	Serial.print('\t');
-	Serial.print(r);
-	Serial.print('\t');
-	Serial.println(e);
-	
-	delay(50);
-}
-
-float bound(float x, float x_min, float x_max){
-	if(x < x_min){x = x_min;}
-	if(x > x_max) {x = x_max;}
-	return x;
-}
 
 /*=====CLASS DEFINITIONS=====*/
-
 class Sweeper     //for claw and for seed dispenser
 {
-  Servo servo;
-  int angle = 0;
-  
+  private:
+    Servo servo;
+    byte lastAngle;
   public:
-    Sweeper(int servoPin){
-		  servo.attach(servoPin);
-	  }
-
-    void Detach()
-    {
-      servo.detach();
+    Sweeper(byte servoPin) {
+      servo.attach(servoPin);
+      reset();
     }
 
-    void Sweep(int fromAngle, int toAngle)
-    {
-      if(fromAngle < toAngle){
-        for(i = fromAngle; i < toAngle; i++){ 
-          servo.write(i);
-        }
+    void reset(){
+      servo.write(90);
+      lastAngle = 90;
+    }
+    
+    void Sweep(int toAngle){
+      if(lastAngle <= toAngle){
+        for(int i = lastAngle; i < toAngle; i++)  {servo.write(i);}
+        lastAngle = toAngle;
       }
-      else if(fromAngle > toAngle){
-        for(i = fromAngle; i > toAngle; i--){
-          servo.write(i);
-        }
+      else if(lastAngle > toAngle){
+        for(int i = lastAngle; i > toAngle; i--)  {servo.write(i);}
+        lastAngle = toAngle;
       }
+    }
+};
+
+class Extender : public Sweeper      //reacher class inherits public fns of Sweeper
+{
+  private:
+    byte forcePin;
+    byte servoPin;
+    byte lastAngle;
+  public:
+    Extender(byte servoPin, byte forcePin):Sweeper(servoPin){
+      forcePin = forcePin;
+    }
+  
+    void Actuate(int lastAngle, int toAngle, int threshold){
+      while(analogRead(forcePin) < threshold){
+			  servo.write(lastAngle);
+        lastAngle++;
+			}
+    }
+
+    void Return(int fromAngle, int toAngle) {Sweep(fromAngle, toAngle);}
+};
+
+class MoistureSensor : public Extender
+{
+  private:
+    byte moistPin;
+    byte reading;
+    unsigned long lastTime;
+  public:
+    MoistureSensor(byte servoPin, byte forcePin, byte moistPin):Extender(servoPin, forcePin){
+      moistPin = moistPin;
+    }
+
+    int Update(){
+      reading = analogRead(moistPin);
+      return reading;
+    }
 };
 
 class Valve
@@ -98,29 +101,24 @@ class Valve
   int valvePin;
 
   public:
-    Valve(int pin){
+    Valve(byte pin){
       valvePin = pin;
     }
     
-    void openValve(){
-      //actuation signal;
-      digitalWrite(valvePin, HIGH);
-    }
+    void openValve()  {digitalWrite(valvePin, HIGH);} //actuation signal
   
-    void closeValve(){
-      //stop signal;
-      digitalWrite(valvePin, LOW);
-    }
+    void closeValve() {digitalWrite(valvePin, LOW);}  //stop signal;
 };
-
+//TO BE COMPLETED
 class Wheel
 {
-  int encoderAPin;
-  int encoderBPin;
-  int pwmSpeed;
+  private:
+    byte encoderAPin;
+    byte encoderBPin;
+    byte pwmSpeed;
 
   public:
-    Wheel(int encoderA, int encoderB){
+    Wheel(byte encoderA, byte encoderB){
       encoderAPin = encoderA;
       encoderBPin = encoderB;
       pinMode(encoderAPin, INPUT);
@@ -131,10 +129,24 @@ class Wheel
     int getSpeed(){
       int rpm;
       //access encoder, run calculations
+      /*y = analogRead(forceClaw);    //store measured force
+        e = r - y;    //e = r - y
+        u_angle = Kp*e;
+        
+        u_angle = bound(u_angle, -180, 180);
+        //myServo.write(u_angle);
+        
+        Serial.print(y);
+        Serial.print('\t');
+        Serial.print(r);
+        Serial.print('\t');
+        Serial.println(e);
+        
+        delay(50);*/
       return rpm;
     }
   
-    void moveForward(//x,y pos){
+    void moveForward(/*x,y pos*/){
       // desired rpm
       int r = 500;
       
@@ -171,30 +183,31 @@ class Wheel
     }
 };
 
-class Extender      //call sweep fn within
-{
-  Servo servo;
-  int angle = 0;
+/*create objects*/
+Sweeper seeder(seedServoPin), claw(clawServoPin);
+Extender digger(diggerPin, forceClaw);
+MoistureSensor moistSensor(moistServo, forceMoist, moisturePin);
+Wheel left(leftEncA, leftEncB), right(rightEncA, rightEncB);
+Valve waterOut(valvePin);
 
-  public:
-    Extender(int servoPin){
-		  servo.attach(servoPin);
-    }
-    
-    void Detach(){
-      servo.detach();
-    }
-    
-    void actuate(int angle){
-      int threshold1, threshold2, threshold3;
-      while(analogRead(forcePin) < threshold1){
-        servo.write(angle);
-        break;
-      }
-      //write angle to servo;
-    }
-  
-    void freeze(){
-      //stop servo
-    }
-};
+/*=====SETUP=====*/
+void setup() {
+	
+	Serial.begin(9600);
+	//myServo.attach(10);
+	//myServo.write(0);
+ 
+	/*~~~INIT~~~*/
+	//pinMode(infraPin, INPUT);
+}
+
+/*=====MAIN=====*/
+void loop() {
+	
+}
+
+float bound(float x, float x_min, float x_max){
+	if(x < x_min){x = x_min;}
+	if(x > x_max) {x = x_max;}
+	return x;
+}
